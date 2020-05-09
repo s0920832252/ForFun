0cd80ff2b9617e78b137aae390ab549efdc4a66b
citychen <s0920832252s@gmail.com>
2020-05-09 16:58:00 +0800

    [TDD 英數字練習] 列出數字測試

--- TDD數字練習/TDD數字練習/UnitTest1.cs
+++ TDD數字練習/TDD數字練習/UnitTest1.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace TDD數字練習
 {
     /// <summary>
-    /// 英數字重構練習專案 : 
+    /// 英數字重構練習專案 : 修改看看
     /// e.g. input A -> output A
     ///      input B -> output B
     ///      input Aa -> output A-Aa
@@ -15,6 +15,12 @@ namespace TDD數字練習
     ///      input ba -> output B-Aa
     ///      input abc -> A-Bb-Ccc
     ///      input 無字母 or null -> 丟例外
+    ///      突然想起來 , 輸入好像可以是數字 ..................................................X你娘的
+    ///      開始補上測試
+    ///      input 1 -> output 1
+    ///      input a1 -> output A-11
+    ///      input 1a -> output 1-Aa
+    ///      input 123 -> output 1-22-333
     /// </summary>
     public class Tests
     {
