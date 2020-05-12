2ef8945055aa5ea99ce86252d191e8a6a903be10
citychen <s0920832252s@gmail.com>
2020-05-13 00:29:31 +0800

    [TDD Account 重構練習]  初始化環境
    使用 Moq & Nunit 作為測試環境
    測試類別 : UnitTest.cs
    
    練習情境 :   試圖重構一隻已經寫好的方法. QueryBudget() in class Accounting
    
    動作
    1. 為 QueryBudget 撰寫測試
        - 思考可能情境
               測試情境
               1. 無效輸入- 查詢日開始必須要小於終止
               2. 查詢整月
               3. 查詢單日
               4. 查詢數日
               5. 查詢跨兩月 e.g. 20200430->20200501
               6. 查詢跨三月 e.g. 20200430->20200601
               7  查詢範圍大於DB 內資料  , 單邊&雙邊
               8. 查詢跨年
        - 讓情境都變成綠燈的測試  (假設方法目前絕對是正確的)
    
    2. 開始重構
        - 盡可能每改一個動作,都跑一次測試看看.
        - 確保這個動作的修改不會影響到方法在原本情境的表現.

---
+++ TDD Account 練習/TDD_AccountSystem_練習/TDD_AccountSystem_練習/Budget.cs
@@ -0,0 +1,8 @@
+﻿namespace TDD_AccountSystem_練習
+{
+    public class Budget
+    {
+        public string  YearMonth { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ TDD Account 練習/TDD_AccountSystem_練習/TDD_AccountSystem_練習/UnitTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+
+namespace TDD_AccountSystem_練習
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void QueryBudget_InvalidInput_GetZero()
+        {
+            var givenBudgets = GivenBudgets(new Budget { });
+            QueryAmountShouldBe(0, new DateTime(2020, 4, 1), new DateTime(2020, 03, 31), givenBudgets);
+        }
+
+
+        [Test]
+        public void QueryBudget_QueryOneMonthInDB_GetBudgetOfBudget()
+        {
+            var accounting = GivenBudgets(new Budget {YearMonth = "202004", Amount = 30});
+            QueryAmountShouldBe(30, DateTime.Parse("2020/04/01"), DateTime.Parse("2020/04/30"), accounting);
+        }
+
+        [Test]
+        public void QueryBudget_QueryOneDay_GetBudgetOfDay()
+        {
+            var givenBudgets = GivenBudgets(new Budget() {YearMonth = "202004", Amount = 30});
+            QueryAmountShouldBe(1, new DateTime(2020, 4, 1), new DateTime(2020, 4, 1), givenBudgets);
+        }
+
+        [Test]
+        public void QueryBudget_QueryDays_GetBudgetOfDays()
+        {
+            var givenBudgets = GivenBudgets(new Budget() {YearMonth = "202004", Amount = 30});
+            QueryAmountShouldBe(7, DateTime.Parse("2020/04/01"), DateTime.Parse("2020/04/07"), givenBudgets);
+        }
+
+        [Test]
+        public void QueryBudget_QueryAcrossTwoMonth_GetBudget()
+        {
+            var givenBudgets = GivenBudgets(new Budget() {YearMonth = "202004", Amount = 30},
+                                            new Budget() {YearMonth = "202005", Amount = 31});
+            QueryAmountShouldBe(2, new DateTime(2020, 4, 30), new DateTime(2020, 5, 1), givenBudgets);
+        }
+
+        [Test]
+        public void QueryBudget_QueryAcrossThirdMonth_GetBudget()
+        {
+            var givenBudgets = GivenBudgets(new Budget() {YearMonth = "202004", Amount = 30},
+                                            new Budget() {YearMonth = "202005", Amount = 31},
+                                            new Budget() {YearMonth = "202006", Amount = 30});
+            QueryAmountShouldBe(33, new DateTime(2020, 4, 30), new DateTime(2020, 6, 1), givenBudgets);
+        }
+
+        [Test]
+        public void QueryBudget_QueryOutSingleRangeOfDB_GetBudgetOfIntersection()
+        {
+            var givenBudgets = GivenBudgets(new Budget() {YearMonth = "202004", Amount = 30});
+            QueryAmountShouldBe(7, new DateTime(2020, 1, 1), new DateTime(2020, 4, 7), givenBudgets);
+            QueryAmountShouldBe(7, new DateTime(2020, 4, 24), new DateTime(2020, 12, 7), givenBudgets);
+        }
+
+        [Test]
+        public void QueryBudget_QueryOutTwoSideRangeOfDb_GetBudgetOfIntersection()
+        {
+            var givenBudgets = GivenBudgets(new Budget() {YearMonth = "202004", Amount = 30});
+            QueryAmountShouldBe(30, new DateTime(2020, 1, 1), new DateTime(2020, 12, 30), givenBudgets);
+        }
+
+        [Test]
+        public void QueryableBudget_QueryYears_GetBudgetOfYears()
+        {
+            var givenBudgets = GivenBudgets(new Budget() {YearMonth = "202004", Amount = 30},
+                                            new Budget() {YearMonth = "202104", Amount = 30},
+                                            new Budget() {YearMonth = "202205", Amount = 31});
+            QueryAmountShouldBe(91, new DateTime(2020, 1, 1), new DateTime(2022, 12, 7), givenBudgets);
+        }
+
+        private static void QueryAmountShouldBe(int        expected, DateTime startDate, DateTime endDate,
+                                                Accounting accounting)
+        {
+            var actual = accounting.QueryBudget(startDate, endDate);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static Accounting GivenBudgets(params Budget[] budgets)
+        {
+            var mock = new Mock<IBudgetRepo>();
+            mock.Setup(s => s.GetAll())
+                .Returns(budgets.ToList());
+            var accounting = new Accounting() {Repo = mock.Object};
+            return accounting;
+        }
+    }
+}
