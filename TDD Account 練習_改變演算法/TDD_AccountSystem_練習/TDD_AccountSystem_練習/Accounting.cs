7933d1b3c40d4b794ee5d52a4b761e7eb8c3a6ea
citychen <s0920832252s@gmail.com>
2020-05-21 00:17:20 +0800

    [ TDD Account 練習 2]  Move Period class into another file

--- TDD Account 練習_改變演算法/TDD_AccountSystem_練習/TDD_AccountSystem_練習/Accounting.cs
+++ TDD Account 練習_改變演算法/TDD_AccountSystem_練習/TDD_AccountSystem_練習/Accounting.cs
@@ -2,43 +2,8 @@
 
 namespace TDD_AccountSystem_練習
 {
-    public class Period
-    {
-        public Period(DateTime startDate, DateTime endDate)
-        {
-            StartDate = startDate;
-            EndDate = endDate;
-        }
-
-        public DateTime StartDate { get; private set; }
-        public DateTime EndDate { get; private set; }
-
-        public bool IsSameWithStart(Budget budget)
-        {
-            return budget.YearMonth == StartDate.ToString("yyyyMM");
-        }
-
-        public bool IsSameWithEnd(Budget budget)
-        {
-            return budget.YearMonth == EndDate.ToString("yyyyMM");
-        }
-
-        public bool InRange(Budget budget)
-        {
-            return StartDate <= budget.CreateDateTime() && EndDate >= budget.CreateDateTime();
-        }
-
-        public bool IsBetweenRange(Budget budget)
-        {
-            return IsSameWithStart(budget) ||
-                   IsSameWithEnd(budget)   ||
-                   InRange(budget);
-        }
-    }
-
     class Accounting
     {
-
         public decimal QueryBudget(DateTime startDate, DateTime endDate)
         {
             if (startDate > endDate)
